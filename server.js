5dbe9eefc9afb8849d475cdf7c2c55ba0a0de995
Malek Abasise <abdmalek.abassisse@esprit.tn>
2024-03-31 06:43:49 +0100

    ticket listing

--- server.js
+++ server.js
@@ -268,11 +268,12 @@ app.get('/stubhubSearch/:query', async (req, res) => {
     const $ = cheerio.load(html);
     console.log($('script').get()[9].attribs['id']);
 
-    const toJson = JSON.parse($('script').get()[9].children[0].data);
-     // Filter items to extract only the URLs
+    const jsonData = $('script[id="index-data"]').first().html();
+    const toJson = JSON.parse(jsonData);     // Filter items to extract only the URLs
      const urls = toJson.eventGrids['2'].items.map(item => item.url);
 
     res.json({
+      json:toJson.eventGrids['2'],
       succes:true,
       data : urls
     });
@@ -282,6 +283,198 @@ app.get('/stubhubSearch/:query', async (req, res) => {
   }
 });
 
+
+app.post('/stubhubSearchTickets', async (req, res) => {
+  const eventURL=req.body.url;
+  const CurrentPage =req.body.CurrentPage;
+  const PageSize = req.body.PageSize;
+  const Quantity = req.body.Quantity;
+  const ShowAllTickets = req.body.ShowAllTickets;
+
+  console.log(eventURL);
+  try {
+    const response = await axios.post(`${eventURL}`,{CurrentPage,PageSize,Quantity,ShowAllTickets}, {
+      rejectUnauthorized: false,
+      proxy: {
+        host: 'brd.superproxy.io',
+        port: '22225',
+        auth: {
+          username: 'brd-customer-hl_0d698af6-zone-unblocker',
+          password: '4rzyw981zlb7'
+        }
+      }
+    });
+
+    const html = response.data;
+       // Filter items to extract only the URLs
+    res.json(html);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.get('/vividseatsSearch/:query', async (req, res) => {
+  const query = req.params.query;
+  const replacedString = query.replace(/ /g, "%20");
+  try {
+    const response = await axios.get(`https://www.vividseats.com/search?searchTerm=${replacedString}`, {
+      rejectUnauthorized: false,
+      proxy: {
+        host: 'brd.superproxy.io',
+        port: '22225',
+        auth: {
+          username: 'brd-customer-hl_0d698af6-zone-unblocker',
+          password: '4rzyw981zlb7'
+        }
+      }
+    });
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const jsonData = $('script[id="__NEXT_DATA__"]').first().html();
+    const toJson = JSON.parse(jsonData);     // Filter items to extract only the URLs
+
+
+    var baseObjs=toJson.props.pageProps.initialProductionListData;
+    var baseObjs2=toJson.props.pageProps.initialAllProductionListData;
+    baseObjs ? baseObjs= baseObjs: baseObjs= baseObjs2
+
+     var urls = baseObjs?.items.map(item => item.id);
+   
+    console.log(urls);
+    res.json({
+      json:baseObjs,
+      succes:true,
+      data : urls
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.get('/vividseatsSearchTickets/:id', async (req, res) => {
+  const eventId=req.params.id;
+
+  try {
+    const response = await axios.get(`https://www.vividseats.com/hermes/api/v1/listings?productionId=${eventId}&includeIpAddress=true`, {
+      rejectUnauthorized: false,
+      proxy: {
+        host: 'brd.superproxy.io',
+        port: '22225',
+        auth: {
+          username: 'brd-customer-hl_0d698af6-zone-unblocker',
+          password: '4rzyw981zlb7'
+        }
+      }
+    });
+
+    const html = response.data;
+       // Filter items to extract only the URLs
+    res.json(html);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+app.get('/gametimeSearch/:query', async (req, res) => {
+  const query=req.params.query;
+
+  try {
+    const response = await axios.get(`https://mobile.gametime.co/v1/search?q=${query}&lat=40.714353&lon=-74.005973&zSearch03=true&zSearch05=alg_v1`, {
+      rejectUnauthorized: false,
+      proxy: {
+        host: 'brd.superproxy.io',
+        port: '22225',
+        auth: {
+          username: 'brd-customer-hl_0d698af6-zone-unblocker',
+          password: '4rzyw981zlb7'
+        }
+      }
+    });
+
+    const html = response.data;
+       // Filter items to extract only the URLs
+    res.json(html);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/getTicketsGametimes', async (req, res) => {
+    const url = req.body.url;
+    console.log(url);
+//  const replacedString = query.replace(/ /g, "%20");
+  try {
+    const response = await axios.get(`${url}`, {
+      rejectUnauthorized: false,
+      proxy: {
+        host: 'brd.superproxy.io',
+        port: '22225',
+        auth: {
+          username: 'brd-customer-hl_0d698af6-zone-unblocker',
+          password: '4rzyw981zlb7'
+        }
+      }
+    });
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    // Find all script tags
+    const scriptTags = $('script');
+
+    // Get the third-to-last script tag
+    const scriptLength = scriptTags.length;
+    const thirdToLastScriptTag = scriptTags.eq(scriptLength - 3);
+
+    // Extract the content of the third-to-last script tag
+    const jsonData = thirdToLastScriptTag.html();
+   // const json = JSON.parse(jsonData);
+
+    const jsonDataStartIndex = jsonData.indexOf('{'); // Find the start index of the JSON object
+    const jsonDataEndIndex = jsonData.lastIndexOf('}'); // Find the end index of the JSON object
+    
+    if (jsonDataStartIndex !== -1 && jsonDataEndIndex !== -1) {
+      const jsonDataString = jsonData.substring(jsonDataStartIndex, jsonDataEndIndex + 1);
+      
+    //  const unescapedJsonData = jsonDataString.replace(/\\"/g, '"'); // Replace escaped double quotes with unescaped double quotes
+     // const jsonDataParsed = JSON.stringify(jsonDataString); // Parse the unescaped JSON string
+      
+      /*const baseObjs = jsonDataParsed.redux.pageProps.initialProductionListData; // Access the data you need
+      const urls = baseObjs?.items.map(item => item.id);
+      console.log(urls);*/
+      const sanitizedData = jsonDataString
+      .replace(/\\u002F/g, '/') // Replace '\\u002F' with '/'
+      .replace(/undefined/g, '"undefined "');      
+      const problematicPart = jsonDataString.substring(180, 250); 
+      console.log(problematicPart); // Output this to see what's around position 219
+
+     // const json = JSON.stringify(sanitizedData);
+  
+
+
+      // Now you can work with jsonData
+      //console.log(json);
+      const json = JSON.parse(sanitizedData);
+      res.json(json)
+    } else {
+      console.error("Unable to find JSON object in the extracted data.");
+      res.status(500).send('Internal Server Error');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
